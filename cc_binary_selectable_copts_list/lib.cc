bf9e2d93d72ec32184dc94befbf03e21bd446b8c
thekyz <thekyz@gmail.com>
2019-07-19 14:37:43 +0200

    Add (wip) list features example

---
+++ cc_binary_selectable_copts_list/lib.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void runlib() {
+#ifdef feature1
+  std::cout << "Building lib with feature 1!\n";
+  #ifdef feature2
+  std::cout << "Building lib with feature 1 and feature 2!\n";
+  #endif
+#elif feature2
+  std::cout << "Building lib with feature 2!\n";
+#else
+  std::cout << "Building lib without features. But the select() in the BUILD "
+            << "file should prevent this case from building in the first "
+            << "place.\n";
+#endif
+}
